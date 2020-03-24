f9ddc14ae44582f1fcbed28689727a5908139385
Tcaprioli <Capriolityler@gmail.com>
2020-03-24 13:57:07 -0400

    added auth. not working

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
   
 
     def create
-        user = User.create(user_params)
-        if user.valid?
-            render json: user
+        @user = User.create(user_params)
+        if @user.valid?
+            @token = encode_token(user_id: @user.id)
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+        else
+            render json: { error: 'failed to create user' }, status: :not_acceptable
         end
-
     end
 
     private
